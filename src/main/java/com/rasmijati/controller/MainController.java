50f349821a5c26292c5d1159c34529265e42cd40
RasmiJati <jatirasmi65@gmail.com>
2024-01-20 18:37:03 +0545

    created user pojo class in model package

---
+++ src/main/java/com/rasmijati/controller/MainController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rasmijati.controller;
+
+import com.rasmijati.model.Gender;
+import com.rasmijati.model.User;
+import java.sql.Date;
+
+/**
+ *
+ * @author rasmi
+ */
+public class MainController {
+
+    public static void main(String[] args) {
+        User u = new User(1L, "rasmi", "rasmi", "rasmi@gmail.com", Date.valueOf("2055-04-05") , Gender.FEMALE);
+        System.out.println(u.toString());
+    }
+}
---
+++ src/main/java/com/rasmijati/model/User.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rasmijati.model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author rasmi
+ */
+public class User {
+
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private Date birthdate;
+    private Gender gender;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String password, String email, Date birthdate, Gender gender) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.birthdate = birthdate;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id + " name: " + name + " password: " + password + " email: " + email + " DOB: " + birthdate + " Gender: " + gender;
+    }
+}
